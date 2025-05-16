b4460840604d15b2dec281f9aa86e7c85c2cedd0
Khadijaz8 <158305799+Khadijaz8@users.noreply.github.com>
2025-05-17 02:01:27 +0500

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const express = require("Express");
+const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const flash = require("connect-flash");
